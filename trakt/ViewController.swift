ab4b1be7fffbbfa0202790a83b41b21ceeeae7ca
rone shender <rone.loza@gmail.com>
2019-11-26 18:18:38 -0500

    first commit

---
+++ trakt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trakt
+//
+//  Created by Lab Positiva Dev on 11/26/19.
+//  Copyright © 2019 Technious. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
